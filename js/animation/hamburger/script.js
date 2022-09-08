8ec1dfce162f2b2c5de8583697a488bde52459d3
P3YCH <96685833+P3YCH@users.noreply.github.com>
2022-09-08 10:28:26 +0700

    Update script.js

--- js/animation/hamburger/script.js
+++ js/animation/hamburger/script.js
@@ -1,1 +1,4 @@
-document.querySelector('.first-button').addEventListener('click', function () {});
+document.querySelector('.first-button').addEventListener('click', function () {
+
+document.querySelector('.animation-hamburger-2').classList.toggle('open');
+});
