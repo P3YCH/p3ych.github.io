481a69ae7d03db7a8e4c5c0cb5cba6e95e5cb01d
P3YCH <96685833+P3YCH@users.noreply.github.com>
2022-09-08 10:12:33 +0700

    Update script.js

--- js/animation/hamburger/2/script.js
+++ js/animation/hamburger/2/script.js
@@ -1,1 +1,2 @@
-eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k[c]||c;k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('0.1(\'.3-4-2\').5.6(\'7\')});',8,8,'document|querySelector||animation|hamburger|classList|toggle|open'.split('|'),0,{}))
+document.querySelector('.animation-hamburger-2').classList.toggle('open');
+});
